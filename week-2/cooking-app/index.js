d800eedbc7560e18cc6fb43863704a8d20bafa96
wwsouthard <wsouthard@my365.bellevue.edu>
2025-11-02 20:33:34 -0600

    chapter 2 work completed

--- week-2/cooking-app/index.js
+++ week-2/cooking-app/index.js
@@ -1,10 +1,24 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Will Southard
+ * Date: 1/12/2025
+ * File Name: index.js
+ * Description: CLI program to demonstrate recipe module functionality
 */
 
-// TODO: Import your module using require
+// Import your module using require
+const recipes = require("./recipes");
 
-// TODO: Implement your CLI program here
+// Implement your CLI program here
+console.log("Welcome to the Recipe Application!");
+console.log("");
+
+// Demonstrate createRecipe function
+console.log(recipes.createRecipe(["flour", "eggs", "milk", "sugar"]));
+console.log("");
+
+// Demonstrate setTimer function
+console.log(recipes.setTimer(30));
+console.log("");
+
+// Demonstrate quit function
+console.log(recipes.quit());
--- week-2/cooking-app/recipes.js
+++ week-2/cooking-app/recipes.js
@@ -1,23 +1,28 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Will Southard
+ * Date: 1/12/2025
+ * File Name: recipes.js
+ * Description: Recipe application module with CommonJS exports
 */
 
 // Define the createRecipe function
 function createRecipe(ingredients) {
-  // TODO: Implement this function
+  return `Recipe created with ingredients: ${ingredients.join(", ")}`;
 }
 
 // Define the setTimer function
 function setTimer(minutes) {
-  // TODO: Implement this function
+  return `Timer set for ${minutes} minutes`;
 }
 
 // Define the quit function
 function quit() {
-  // TODO: Implement this function
+  return "Program exited";
 }
 
-// TODO: Export the functions
+// Export the functions
+module.exports = {
+  createRecipe,
+  setTimer,
+  quit
+};
